004e3f05f5019d19651352969b5ea16647230017
intervalia <intervalia@gmail.com>
2020-05-25 19:22:58 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  environments: {
+    api: {
+      globals: {
+        HttpError: true,
+        HttpResponse: true,
+        throw404: true,
+        HttpError: true,
+        apimodule: true
+      }
+    }
+  }
+};
